9face9720017eb35db8b90077eeda81c726d8ce9
Trevor Bekolay <tbekolay@gmail.com>
2016-07-06 15:22:39 -0400

    Prevent the 'no handlers' message from logging
    When using recurrent with Python 2.7, the logging module will print
    the message 'No handlers could be found for logger "recurrent"'.
    This occurs when not handlers have been set up in the logging module,
    which is the case when importing recurrent in an interpreter, for
    example.
    
    This commit adds a NullHandler, which does nothing when other handlers
    exist, and suppresses the message if no other handlers exist.

--- src/recurrent/__init__.py
+++ src/recurrent/__init__.py
@@ -1,3 +1,12 @@
+import logging
+
+log = logging.getLogger(__name__)
+try:
+    # Prevent output if no handler set
+    log.addHandler(logging.NullHandler())
+except AttributeError:
+    pass
+
 from event_parser import RecurringEvent
 
 def parse(s, now=None):
